f1c86f3534f3046b434496a94e193e86642be257
khangvtran <vinhkhangtran09@gmail.com>
2017-12-05 13:45:58 -0800

    main.h modified

--- Recipe_DataStructure/src/main.h
+++ Recipe_DataStructure/src/main.h
@@ -49,11 +49,90 @@ void printStringVector(ostream& out, vector<string> v);
 
 
 
+
+
+void idCreator(Recipe& recipe, HashTable& hashTable1) {
+	wordID one;
+	for (unsigned int i = 0; i < recipe.get_keys().size(); i++) {
+		one.set_Word(recipe.get_keys()[i]);
+		hashTable1.IDinsert(one);
+	}
+
+}
+
+void readAndStore(ifstream& fin, string& nonKeys, BST<Recipe>& bst,
+		HashTable& idHT) {
+	string name, category, flavor, buf;
+	string ingredients = "";
+	string direction = "";
+	unsigned time, difficulty;
+	vector<string> keys;
+
+	int count = 0;
+	while (getline(fin, name)) {
+		count++;
+		//cout << count << "." << endl;
+
+		getline(fin, category);
+		getline(fin, flavor);
+
+		// read and concatenate ingredients until hitting a #
+		do {
+			getline(fin, buf);
+			if (buf.find("#") == string::npos)
+				ingredients += buf;
+		} while (buf.find("#") == string::npos);
+
+		getline(fin, buf);
+		time = stoi(buf);
+
+		getline(fin, buf);
+		difficulty = stoi(buf);
+
+		// read and concatenate to direction until hitting a #
+		do {
+			getline(fin, buf);
+			if (buf.find("#") == string::npos)
+				direction += buf;
+		} while (buf.find("#") == string::npos);
+
+		// get the blankspace
+		getline(fin, buf);
+
+		// create the keys vector
+
+		// Create the recipe object
+		Recipe recipe(name, category, flavor, ingredients, time, difficulty,
+				direction);
+		setKeysForObject(recipe, nonKeys); // the object key was empty until we setKeys
+
+		//printStringVector(cout, recipe.get_keys());
+		//cout << endl
+		//<< "**************************************************************"
+		//<< endl;
+
+		// Insert the object into the
+		bst.insert(recipe);
+		idCreator(recipe, idHT);
+		idHT.BSTinsert(recipe);
+
+		// reset Ingredients and Direction for the next recipe to be read in
+		ingredients = "";
+		direction = "";
+	}
+
+}
+
+
+
+
 void readAndAppend(ifstream& fin, string& str) {
 	string buf;
 	while (fin >> buf)
 		str += " " + buf;
 }
+
+
 
 void setKeysForObject(Recipe& recipe, string& nonKeys) {
 	vector<string> keys;
@@ -64,6 +143,9 @@ void setKeysForObject(Recipe& recipe, string& nonKeys) {
 	getKeysFromString(recipe.get_direction(), nonKeys, keys);
 	recipe.set_keys(keys);
 }
+
+
+
 
 void getKeysFromString(string field, string nonKeys, vector<string>& keys) {
 	istringstream iss(field);
@@ -90,11 +172,16 @@ void getKeysFromString(string field, string nonKeys, vector<string>& keys) {
 
 }
 
+
+
+
 void printStringVector(ostream& out, vector<string> v) {
 	for (unsigned int i = 0; i < v.size(); i++)
 		out << v[i] << " ";
 	out << endl;
 }
+
+
 
 
 
